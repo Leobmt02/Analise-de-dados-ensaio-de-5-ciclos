210439ff55f029feeb29c6baab6cd385bdb84ea7
Léo <150980547+Leobmt02@users.noreply.github.com>
2025-02-17 16:48:08 -0300

    Update appCerto.py

--- appCerto.py
+++ appCerto.py
@@ -36,7 +36,7 @@ if arquivo:
 
         # Criar gráfico interativo
         st.subheader("📈 Gráfico de Dados")
-        fig, ax = plt.subplots(figsize=(6, 4))
+        fig, ax = plt.subplots(figsize=(4, 4))
         ax.plot(df[col_x], df[col_y], marker='o', linestyle='-', color='b', label=f'{col_x} x {col_y}')
 
         # Configuração do gráfico
