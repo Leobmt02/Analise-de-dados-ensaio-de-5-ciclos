cd7b8ae4eb78448d91c1074f1ecc530e6c181924
Léo <150980547+Leobmt02@users.noreply.github.com>
2025-03-25 08:57:06 -0300

    Update app5ciclos.py

--- app5ciclos.py
+++ app5ciclos.py
@@ -3,32 +3,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Configuração da página
-st.set_page_config(page_title="Analisador de Dados - Excel", layout="wide")
+st.set_page_config(page_title="Analisador de Dados - Excel e CSV", layout="wide")
 
 # Título da aplicação
 st.title("📊 Ensaios de Teste de 5 Ciclos")
 
 # Upload do arquivo
-arquivo = st.file_uploader("Selecione um arquivo Excel", type=["xlsx", "xls"])
+arquivo = st.file_uploader("Selecione um arquivo Excel ou CSV", type=["xlsx", "xls", "csv"])
 
 if arquivo:
     try:
-        # Obtendo os nomes das planilhas
-        xls = pd.ExcelFile(arquivo, engine='openpyxl')
-        planilhas = xls.sheet_names
+        # Verificar a extensão do arquivo
+        if arquivo.name.endswith('.csv'):
+            df = pd.read_csv(arquivo, skiprows=7)
+        else:
+            # Obtendo os nomes das planilhas
+            xls = pd.ExcelFile(arquivo, engine='openpyxl')
+            planilhas = xls.sheet_names
 
-        # Se houver mais de uma aba, permitir seleção
-        aba_selecionada = st.selectbox("Selecione a aba:", planilhas)
+            # Se houver mais de uma aba, permitir seleção
+            aba_selecionada = st.selectbox("Selecione a aba:", planilhas)
 
-        # Carregando dados da aba selecionada
-        df = pd.read_excel(xls, sheet_name=aba_selecionada, skiprows=7)
+            # Carregando dados da aba selecionada
+            df = pd.read_excel(xls, sheet_name=aba_selecionada, skiprows=7)
 
         # Exibir as colunas encontradas
         #st.write("📌 **Colunas disponíveis:**", df.columns.tolist())
 
         # Permitir ao usuário selecionar as colunas desejadas
-        col_x = st.selectbox("Selecione a coluna para o eixo X:", ['mm'])
-        col_y = st.selectbox("Selecione a coluna para o eixo Y:", ['kN'])
+        col_x = st.selectbox("Selecione a coluna para o eixo X:", df.columns)
+        col_y = st.selectbox("Selecione a coluna para o eixo Y:", df.columns)
 
         # Exibir as primeiras linhas do DataFrame
         #st.subheader("📋 Dados Carregados")
